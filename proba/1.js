dd6d0ac7ad0b3c36fed66a41abc095394844ea3d
Anatoliy_Iliev <vekok12@gmail.com>
2021-11-29 14:11:08 +0200

    cw11

--- proba/1.js
+++ proba/1.js
@@ -1,17 +1,15 @@
 // Колбэк-функция
 function greet(name) {
   console.log(`Добро пожаловать ${name}.`);
+}
 
-};
-  
 // Функция высшего порядка
 function registerGuest(name, callback) {
   console.log(`Регистрируем гостя ${name}.`);
   callback(name);
-};
-  
+}
 
-registerGuest('Манго', greet)
+registerGuest('Манго', greet);
 
 /* 
 console.log(); 

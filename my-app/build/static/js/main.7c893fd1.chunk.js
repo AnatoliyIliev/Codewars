073c34ac712f764f122c415751353a5ff5858774
Anatoliy Iliev <vekok12@gmail.com>
2021-11-29 22:58:09 +0200

    cw

---
+++ my-app/build/static/js/main.7c893fd1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],[function(p,s,o){"use strict";o.r(s);console.log([30,20,40,50,50],[30,20,40,50,50],[50,4545.545454,54.455544],[30,20,40,50,50])}],[[0,1]]]);
+//# sourceMappingURL=main.7c893fd1.chunk.js.map

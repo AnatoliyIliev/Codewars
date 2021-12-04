ebd73827668f14301833ca155a792b8e280ccb20
Anatoliy Iliev <vekok12@gmail.com>
2021-12-04 22:32:30 +0200

    cw16

--- codewars/codewars-16.js
+++ codewars/codewars-16.js
@@ -1,24 +1,35 @@
 function isPangram(string) {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
   const text = string.toLowerCase().split(' ').join('').split('');
-  text.pop();
-
   console.log(text);
-  const find = alphabet.some(alp => {
-    return text.find(t => {
+  let acc = [];
+  alphabet.some(alp => {
+    text.find(t => {
       if (alp === t) {
-        return;
+        acc.push(t);
+        return acc.length;
       }
     });
   });
-  return find;
+  if (acc.length === 26) {
+    return true;
+  } else return false;
 }
 
 var string = 'The quick brown fox jumps over the lazy dog.';
 console.log(isPangram(string)); // true
 
-// var string2 = 'This is not a pangram.';
-// console.log(isPangram(string2)); // false
+var string2 = 'This is not a pangram.';
+console.log(isPangram(string2)); // false
+
+var string3 = 'Cwm fjord bank glyphs vext quiz';
+console.log(isPangram(string3)); // false
+
+var string4 = 'ABCD45EFGH,IJK,LMNOPQR56STUVW3XYZ';
+console.log(isPangram(string4)); // false
+
+var string5 = 'AbCdEfGhIjKlM zYxWvUtSrQpOn';
+console.log(isPangram(string5)); // false
 
 /*
 A pangram is a sentence that contains every single letter 

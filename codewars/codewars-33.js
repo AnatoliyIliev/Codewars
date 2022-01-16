4e1ed7a52bcd708439ddcfd23fe48d2cc1f8c81d
Anatoliy Iliev <vekok12@gmail.com>
2022-01-16 22:36:33 +0200

    start cw-33

---
+++ codewars/codewars-33.js
@@ -0,0 +1,22 @@
+function findMissing(arr1, arr2) {
+  // Your solution here
+}
+
+console.log(findMissing([1, 2, 3], [1, 3])); //, 2);
+console.log(findMissing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2])); //, 8);
+console.log(findMissing([7], [])); //, 7);
+console.log(findMissing([4, 3, 3, 61, 8, 8], [8, 61, 8, 3, 4])); //, 3);
+console.log(findMissing([0, 0, 0, 0, 0], [0, 0, 0, 0])); //, 0);
+
+/*
+Given two integer arrays where the second array is a shuffled 
+duplicate of the first array with one element missing, find the missing element.
+
+Please note, there may be duplicates in the arrays, 
+so checking if a numerical value exists in one 
+and not the other is not a valid solution.
+
+find_missing([1, 2, 2, 3], [1, 2, 3]) => 2
+find_missing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]) => 8
+The first array will always have at least one element.
+*/

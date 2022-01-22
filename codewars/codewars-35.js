1d8a4ef8531ee4cb515a7d3beff35b82c82cf925
AnatoliyIliev <vekok12@gmail.com>
2022-01-22 21:59:53 +0200

    finish cw-35

--- codewars/codewars-35.js
+++ codewars/codewars-35.js
@@ -1,26 +1,21 @@
 function maxTriSum(numbers) {
-  let a = numbers.filter((number, idx) => numbers.indexOf(number) === idx);
-  console.log(a);
-  let b = a.filter(n => n >= 3);
-  console.log(b);
-
-  // return numbers
-  //   .filter((number, idx) => numbers.indexOf(number) === idx)
-  //   .filter(n => Math.abs(n) >= 3)
-  //   .slice(0, 3)
-  //   .reduce((previousValue, currentValue) => previousValue + currentValue);
+  return numbers
+    .sort((a, b) => b - a)
+    .filter((number, idx) => numbers.indexOf(number) === idx)
+    .slice(0, 3)
+    .reduce((previousValue, currentValue) => previousValue + currentValue);
 }
 
-// console.log(maxTriSum([3, 2, 6, 8, 2, 3])); //, 17);
-// console.log(maxTriSum([2, 9, 13, 10, 5, 2, 9, 5])); //, 32);
-// console.log(maxTriSum([2, 1, 8, 0, 6, 4, 8, 6, 2, 4])); //, 18);
+console.log(maxTriSum([3, 2, 6, 8, 2, 3])); //, 17);
+console.log(maxTriSum([2, 9, 13, 10, 5, 2, 9, 5])); //, 32);
+console.log(maxTriSum([2, 1, 8, 0, 6, 4, 8, 6, 2, 4])); //, 18);
 console.log(maxTriSum([-3, -27, -4, -2, -27, -2])); //, -9);
-// console.log(maxTriSum([-14, -12, -7, -42, -809, -14, -12])); //, -33);
-// console.log(maxTriSum([-13, -50, 57, 13, 67, -13, 57, 108, 67])); //, 232);
-// console.log(maxTriSum([-7, 12, -7, 29, -5, 0, -7, 0, 0, 29])); //, 41);
-// console.log(maxTriSum([-2, 0, 2])); //, 0);
-// console.log(maxTriSum([-2, -4, 0, -9, 2])); //, 0);
-// console.log(maxTriSum([-5, -1, -9, 0, 2])); //, 1);
+console.log(maxTriSum([-14, -12, -7, -42, -809, -14, -12])); //, -33);
+console.log(maxTriSum([-13, -50, 57, 13, 67, -13, 57, 108, 67])); //, 232);
+console.log(maxTriSum([-7, 12, -7, 29, -5, 0, -7, 0, 0, 29])); //, 41);
+console.log(maxTriSum([-2, 0, 2])); //, 0);
+console.log(maxTriSum([-2, -4, 0, -9, 2])); //, 0);
+console.log(maxTriSum([-5, -1, -9, 0, 2])); //, 1);
 
 /*
 Task

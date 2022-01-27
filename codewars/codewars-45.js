23703fc752364f9a072863f2139f08e4ea85cf54
AnatoliyIliev <vekok12@gmail.com>
2022-01-27 21:25:22 +0200

    cw-45

---
+++ codewars/codewars-45.js
@@ -0,0 +1,41 @@
+function dataReverse(data) {
+  let d = [];
+  for (let i = 0; i + 7 < data.length; i += 8) {
+    d.unshift(...data.slice(i, i + 8));
+  }
+  return d;
+}
+const data1 = [
+  1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0,
+  1, 0, 1, 0, 1, 0,
+];
+const data2 = [
+  1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+  1, 1, 1, 1, 1, 1,
+];
+console.log(dataReverse(data1)); //, data2);
+
+const data3 = [0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1];
+const data4 = [0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0];
+console.log(dataReverse(data3)); // data4);
+
+/*
+A stream of data is received and needs to be reversed.
+
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+
+10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+The total number of bits will always be a multiple of 8.
+
+The data is given in an array as such:
+
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+Note: In the C and NASM languages you are given the third parameter which is the number of segment blocks.
+
+FUNDAMENTALSARRAYS
+*/

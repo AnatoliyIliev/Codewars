cbbd9d88280a470bfe48f9b1699d0494a6539cfa
AnatoliyIliev <vekok12@gmail.com>
2022-01-28 23:43:18 +0200

    cw-46

--- codewars/codewars-46.js
+++ codewars/codewars-46.js
@@ -2,16 +2,20 @@ function strCount(obj) {
   let res = 0;
   Object.values(obj).forEach(a => {
     if (Array.isArray(a)) {
-      console.log(a);
+      console.log('Array -->', a);
       if (a !== 0) {
         a.forEach(b => {
           if (typeof b === 'string') {
+            console.log('string -->', b);
             res += 1;
           }
         });
       }
     } else if (typeof a === 'string') {
       res += 1;
+    } else if (typeof a === 'object') {
+      console.log('object -->', a);
+      // console.log(Object.values(a));
     }
   });
   return res;
@@ -25,10 +29,85 @@ console.log(
     fourth: ['anytime', 2, 3, 4],
     fifth: null,
     sixth: undefined,
-    seventh: {},
+    seventh: { three: 'z', four: 'd' },
+    eight: [1, 2, 3, 4, '0', { one: 'a', two: 's' }],
+    ten: ['and another', 2, 'and again', [undefined, 'and again'], false],
+    eleven: {},
   }),
-); //,3,"Did not count the correct number of strings. Check counting inside nested objects.");
+); //,11,"Did not count the correct number of strings. Check counting inside nested objects.");
 
+/*
+console.log(strCount({{
+  first: '1',
+  second: '2',
+  third: false,
+  fourth: [ 'anytime', 2, 3, 4 ],
+  fifth: null,
+  sixth: undefined,
+  seventh: {}
+}
+[ '1', 2, '3', '4' ]
+[
+  '1',
+  2,
+  '3',
+  [ 'and another', 2, 'and again', [ undefined, 'and again' ], false ],
+  '4'
+]})); //expected 5 to equal 6
+
+console.log(strCount({[
+  'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin',
+  'strin'
+]
+[
+  'strin', 'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin', 'strin',
+  'strin', 'strin', 'strin', 'strin',
+  'strin'
+]
+[
+  'strin',
+  [ 1, 2, 3, 4, '0', { one: 'a', two: 's' } ],
+  'strin',
+  [ 1, 2, 3, 4, '0', { one: 'a', two: 's' } ],
+  'strin',
+  [ 1, 2, 3, 4, '0', { one: 'a', two: 's' } ],
+  'strin',
+  [ 1, 2, 3, 4, '0', { one: 'a', two: 's' } ],
+  'strin',
+  [ 1, 2, 3, 4, '0', { one: 'a', two: 's' } ],
+  'strin',
+  [ 1, 2, 3, 4, '0', { one: 'a', two: 's' } ],
+  'strin',
+  [ 1, 2, 3, 4, '0', { one: 'a', two: 's' } ],
+  'strin',
+  [ 1, 2, 3, 4, '0', { one: 'a', two: 's' } ],
+  'strin',
+  [ 1, 2, 3, 4, '0', { one: 'a', two: 's' } ],
+  'strin',
+  [ 1, 2, 3, 4, '0', { one: 'a', two: 's' } ],
+  'strin',
+  [ 1, 2, 3, 4, '0', { one: 'a', two: 's' } ],
+  'strin',
+  [ 1, 2, 3, 4, '0', { one: 'a', two: 's' } ],
+  'strin'
+]})); //expected 25 to equal 49
+*/
 /*
 Create a function strCount (takes an object as argument) that will count all string values inside an object. For example:
 

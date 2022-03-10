067db2d397de6e19587c2c55e0599e093237b68d
AnatoliyIliev <vekok12@gmail.com>
2022-03-10 22:33:13 +0200

    cw-66

--- codewars/codewars-66.js
+++ codewars/codewars-66.js
@@ -1,27 +1,52 @@
 var validWord = function (dictionary, word) {
-  let spl = word;
-  dictionary.forEach(w => {
-    spl = spl.split(w).join('');
-  });
-  return spl;
-  // return spl ? false : true;
+  for (let i = 0; i < dictionary.length; i += 1) {
+    for (let j = 0; j < word.length; j += 1) {
+      console.log(dictionary[i], '=', word.slice(0, j));
+      if (dictionary[i] == word.slice(0, j)) {
+        word.splice(0, j);
+        console.log(word.slice(0, j));
+        // word.slice(0, j)
+      }
+    }
+  }
 };
 
 console.log(validWord(['code', 'wars'], 'codewars')); //, true]);
-console.log(validWord(['wars', 'code'], 'codewars')); //, true]);
-console.log(validWord(['code', 'wars'], 'codecodewars')); //, true]);
-console.log(validWord(['code', 'wars'], 'codewar')); //, false]);
-console.log(validWord(['code', 'wars'], 'codewarscode')); //, true]);
-console.log(validWord(['code', 'star', 'wars'], 'starwars')); //, true]);
-console.log(validWord(['Star', 'Code', 'Wars'], 'StarCodeWars')); //, true]);
-console.log(validWord(['Star', 'Code', 'Wars'], 'WarsStarCode')); //, true]);
-console.log(validWord(['Star', 'Code', 'Wars'], 'CodeStarsWar')); //, false]);
-console.log(validWord([], 'codewars')); //, false]);
-console.log(validWord(['code', 'wars'], 'code')); //, true]);
-console.log(validWord(['a', 'b', 'c', 'd', 'e', 'f'], 'abcdef')); //, true]);
-console.log(validWord(['a', 'b', 'c', 'd', 'e', 'f'], 'abcdefg')); //, false]);
-console.log(validWord(['ab', 'a', 'bc'], 'abc')); //, true]);
-console.log(validWord(['ab', 'bc'], 'abc')); //, false]);
+// console.log(validWord(['wars', 'code'], 'codewars')); //, true]);
+// console.log(validWord(['code', 'wars'], 'codecodewars')); //, true]);
+// console.log(validWord(['code', 'wars'], 'codewar')); //, false]);
+// console.log(validWord(['code', 'wars'], 'codewarscode')); //, true]);
+// console.log(validWord(['code', 'star', 'wars'], 'starwars')); //, true]);
+// console.log(validWord(['Star', 'Code', 'Wars'], 'StarCodeWars')); //, true]);
+// console.log(validWord(['Star', 'Code', 'Wars'], 'WarsStarCode')); //, true]);
+// console.log(validWord(['Star', 'Code', 'Wars'], 'CodeStarsWar')); //, false]);
+// console.log(validWord([], 'codewars')); //, false]);
+// console.log(validWord(['code', 'wars'], 'code')); //, true]);
+// console.log(validWord(['a', 'b', 'c', 'd', 'e', 'f'], 'abcdef')); //, true]);
+// console.log(validWord(['a', 'b', 'c', 'd', 'e', 'f'], 'abcdefg')); //, false]);
+// console.log(validWord(['ab', 'a', 'bc'], 'abc')); //, true]);
+// console.log(validWord(['ab', 'bc'], 'abc')); //, false]);
+// console.log(
+//   validWord(
+//     [
+//       'jwdz',
+//       'f',
+//       'bfcn',
+//       'cg',
+//       'sh',
+//       'h',
+//       'wyp',
+//       'j',
+//       'scw',
+//       'py',
+//       'syn',
+//       'shy',
+//       'pylw',
+//       'shy',
+//     ],
+//     'jwdzfbfcncgshhwypjscw',
+//   ),
+// ); // true
 
 /*
 You are given a sequence of valid words and a string. 
@@ -46,5 +71,17 @@ var validWord = function (dictionary, word) {
     spl = spl.split(w).join('');
   });
   return spl ? false : true;
+};
+
+
+var validWord = function (dictionary, word) {
+  let spl = word;
+  dictionary.forEach(w => {
+    spl = spl.split(w).join('');
+  });
+
+  if (!spl) return true;
+  else if (dictionary.join('') === word) return true;
+  // else return false;
 };
 */

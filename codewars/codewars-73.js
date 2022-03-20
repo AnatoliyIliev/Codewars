2a3823f36cbe2f1aa3135177e2d2df17a8aaf7ee
AnatoliyIliev <vekok12@gmail.com>
2022-03-20 13:25:10 +0200

    cw-73

---
+++ codewars/codewars-73.js
@@ -0,0 +1,35 @@
+function createFunctions(n) {
+  var callbacks = [];
+
+  for (let i = 0; i < n; i++) {
+    callbacks.push(function () {
+      return i;
+    });
+  }
+
+  return callbacks;
+}
+
+// console.log(createFunctions(0));
+console.log(createFunctions(3));
+// console.log(createFunctions(5));
+// console.log(createFunctions(10));
+// console.log(createFunctions(109));
+
+/*
+
+We want to create a function, which returns an array of functions, 
+which return their index in the array. For better understanding, here an example:
+
+var callbacks = createFunctions(5); 
+// create an array, containing 5 functions
+
+callbacks[0](); // must return 0
+callbacks[3](); // must return 3
+We already implemented that function, 
+but when we actually run the code, 
+the result doesn't look like what we expected. 
+Can you spot, what's wrong with it? 
+A test fixture is also available
+
+*/
